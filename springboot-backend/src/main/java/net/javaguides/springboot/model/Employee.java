dbbb2722654628af7c6d2a3fae9827295aa964f0
saura <saura@DESKTOP-PLU7G4S>
2023-05-07 02:37:57 +0530

    initial Commit.

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,69 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	//private static final boolean TRUE = false;
+
+	//private static final boolean FALSE = false;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@JsonProperty("firstname")
+	private String firstName;
+	
+	@JsonProperty("lastname")
+	private String lastName;
+	
+	@JsonProperty("email")
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
